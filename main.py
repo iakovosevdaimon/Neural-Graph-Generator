9191e175ec7569f0950315f9cc1e39590e972efe
Giannis Nikolentzos <giannisnik@hotmail.com>
2023-07-06 15:34:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import networkx as nx
 import numpy as np
 
@@ -5,13 +6,12 @@ import torch
 import torch.nn as nn
 from torch import _VF
 import torch.nn.functional as F
-from torch.nn.utils.rnn import pad_packed_sequence,PackedSequence,pad_packed_sequence
 from torch_geometric.nn import GINConv
 from torch_geometric.loader import DataLoader
 from torch_scatter import scatter_add
 
 from softsort import SoftSort
-from utils import create_dataset
+from utils import create_dataset, linear_beta_schedule
 
 
 # Hyperparameters
@@ -23,7 +23,7 @@ n_layers = 2
 n_nodes = 50
 
 # Construct dataset
-Gs = [nx.cycle_graph(i) for i in range(20, 51)]+[nx.path_graph(i) for i in range(20, 51)]+[nx.star_graph(i) for i in range(19, 50)]
+Gs = [nx.ladder_graph(i) for i in range(10, 26)] + [nx.wheel_graph(i) for i in range(20, 51)] + [nx.cycle_graph(i) for i in range(20, 51)]+[nx.path_graph(i) for i in range(20, 51)]+[nx.star_graph(i) for i in range(19, 50)]
 data = create_dataset(Gs, pos_enc_dim, n_nodes)
 # TO ADD: write to pickle file here
 
@@ -34,9 +34,9 @@ val_dataset = [data[idx[i]] for i in range(int(0.8*len(data)), int(0.9*len(data)
 test_dataset = [data[idx[i]] for i in range(int(0.9*len(data)), len(data))]
 
 # Dataloaders
-train_loader = DataLoader(train_dataset, batch_size=32)
-val_loader = DataLoader(val_dataset, batch_size=32)
-test_loader = DataLoader(test_dataset, batch_size=32)
+train_loader = DataLoader(train_dataset, batch_size=64)
+val_loader = DataLoader(val_dataset, batch_size=64)
+test_loader = DataLoader(test_dataset, batch_size=64)
 
 # Encoder
 class Encoder(nn.Module):
@@ -92,7 +92,7 @@ class Decoder(nn.Module):
         return adj
 
 
-# Decoder
+# Autoencoder
 class AutoEncoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, n_layers, n_nodes):
         super(AutoEncoder, self).__init__()
@@ -105,6 +105,14 @@ class AutoEncoder(nn.Module):
 
     def forward(self, data):
         x_g, _ = self.encoder(data.x, data.edge_index, data.batch)
+        adj = self.decoder(x_g)
+        return adj
+
+    def encode(self, data):
+        x_g, _ = self.encoder(data.x, data.edge_index, data.batch)
+        return x_g
+
+    def decode(self, x_g):
         adj = self.decoder(x_g)
         return adj
 
@@ -150,37 +158,153 @@ class AutoEncoder(nn.Module):
         
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = AutoEncoder(pos_enc_dim, hidden_dim, n_layers, n_nodes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+autoencoder = AutoEncoder(pos_enc_dim, hidden_dim, n_layers, n_nodes).to(device)
+optimizer = torch.optim.Adam(autoencoder.parameters(), lr=lr)
 
-
-def train(epoch, loader, optimizer):
-    model.train()
-    loss_all = 0
-
-    for data in loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        loss = F.nll_loss(model(data), data.y)
-        loss.backward()
-        loss_all += data.num_graphs * loss.item()
-        optimizer.step()
-    return loss_all / len(loader.dataset)
-
-
-
-best_val_loss, test_acc = 100, 0
+# Train autoencoder
 for epoch in range(1, epochs+1):
-    model.train()
     loss_all = 0
 
     for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
-        loss = model.loss_function(data)
+        loss = autoencoder.loss_function(data)
         loss.backward()
         loss_all += data.num_graphs * loss.item()
         optimizer.step()
     if epoch % 5 == 0:
+        print('Epoch: {:03d}, Train Loss: {:.7f}'.format(
+                epoch, loss_all))
+
+#################################################################################
+
+def extract(a, t, x_shape):
+    batch_size = t.shape[0]
+    out = a.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+
+
+# forward diffusion (using the nice property)
+def q_sample(x_start, t, noise=None):
+    if noise is None:
+        noise = torch.randn_like(x_start)
+
+    sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape)
+    sqrt_one_minus_alphas_cumprod_t = extract(
+        sqrt_one_minus_alphas_cumprod, t, x_start.shape
+    )
+
+    return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+
+
+# Loss function for denoising
+def p_losses(denoise_model, x_start, t, noise=None, loss_type="l1"):
+    if noise is None:
+        noise = torch.randn_like(x_start)
+
+    x_noisy = q_sample(x_start=x_start, t=t, noise=noise)
+    predicted_noise = denoise_model(x_noisy, t)
+    
+    if loss_type == 'l1':
+        loss = F.l1_loss(noise, predicted_noise)
+    elif loss_type == 'l2':
+        loss = F.mse_loss(noise, predicted_noise)
+    elif loss_type == "huber":
+        loss = F.smooth_l1_loss(noise, predicted_noise)
+    else:
+        raise NotImplementedError()
+
+    return loss
+
+
+# Position embeddings
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+
+
+# Denoise model
+class DenoiseNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers):
+        super(DenoiseNN, self).__init__()
+        self.n_layers = n_layers
+
+        self.time_mlp = nn.Sequential(
+            SinusoidalPositionEmbeddings(hidden_dim),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, hidden_dim),
+        )
+
+        mlp_layers = [nn.Linear(input_dim, hidden_dim)] + [nn.Linear(hidden_dim, hidden_dim) for i in range(n_layers-2)]
+        mlp_layers.append(nn.Linear(hidden_dim, input_dim))
+        self.mlp = nn.ModuleList(mlp_layers)
+
+        bn_layers = [nn.BatchNorm1d(hidden_dim) for i in range(n_layers-1)]
+        self.bn = nn.ModuleList(bn_layers)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x, t):
+        t = self.time_mlp(t)
+        for i in range(self.n_layers-1):
+            x = self.relu(self.mlp[i](x))+t
+            x = self.bn[i](x)
+        
+        x = self.mlp[self.n_layers-1](x)
+        return x
+
+timesteps = 300
+
+# define beta schedule
+betas = linear_beta_schedule(timesteps=timesteps)
+
+# define alphas 
+alphas = 1. - betas
+alphas_cumprod = torch.cumprod(alphas, axis=0)
+alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+
+# calculations for diffusion q(x_t | x_{t-1}) and others
+sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+
+# calculations for posterior q(x_{t-1} | x_t, x_0)
+posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+epochs = 5000
+
+denoise_model = DenoiseNN(input_dim=hidden_dim, hidden_dim=256, n_layers=3).to(device)
+optimizer = torch.optim.Adam(denoise_model.parameters(), lr=0.001)
+
+# Train denoising model
+for epoch in range(epochs):
+    loss_all = 0
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+
+        batch_size = data.num_graphs
+        data = data.to(device)
+
+        x_g = autoencoder.encode(data)
+
+        t = torch.randint(0, timesteps, (batch_size,), device=device).long()
+        loss = p_losses(denoise_model, x_g, t, loss_type="huber")
+        loss.backward()
+        loss_all += data.num_graphs * loss.item()
+        optimizer.step()
+
+    if epoch % 100 == 0:
         print('Epoch: {:03d}, Train Loss: {:.7f}'.format(
                 epoch, loss_all))
