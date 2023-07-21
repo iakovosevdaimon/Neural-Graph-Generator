e7a61a7acb45fb671216629f43e2444098e00803
Giannis Nikolentzos <giannisnik@hotmail.com>
2023-07-21 15:38:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,273 +1,122 @@
-import math
+import argparse
 import networkx as nx
 import numpy as np
 
 import torch
 import torch.nn as nn
-from torch import _VF
 import torch.nn.functional as F
-from torch_geometric.nn import GINConv
-from torch_geometric.loader import DataLoader
-from torch_scatter import scatter_add
+from torch.utils.data import DataLoader
 
-from softsort import SoftSort
-from utils import create_dataset, linear_beta_schedule
+from autoencoder import AutoEncoder, VariationalAutoEncoder
+from denoise_model import DenoiseNN, p_losses, sample
+from utils import create_dataset, CustomDataset, linear_beta_schedule
 
+np.random.seed(13)
 
-# Hyperparameters
-pos_enc_dim = 10
-epochs = 200
-lr = 0.001
-hidden_dim = 32
-n_layers = 2
-n_nodes = 50
-
-# Construct dataset
-Gs = [nx.ladder_graph(i) for i in range(10, 26)] + [nx.wheel_graph(i) for i in range(20, 51)] + [nx.cycle_graph(i) for i in range(20, 51)]+[nx.path_graph(i) for i in range(20, 51)]+[nx.star_graph(i) for i in range(19, 50)]
-data = create_dataset(Gs, pos_enc_dim, n_nodes)
-# TO ADD: write to pickle file here
-
-# Slit into training, validation and test sets
-idx = np.random.permutation(len(data))
-train_dataset = [data[idx[i]] for i in range(int(0.8*len(data)))]
-val_dataset = [data[idx[i]] for i in range(int(0.8*len(data)), int(0.9*len(data)))]
-test_dataset = [data[idx[i]] for i in range(int(0.9*len(data)), len(data))]
-
-# Dataloaders
-train_loader = DataLoader(train_dataset, batch_size=64)
-val_loader = DataLoader(val_dataset, batch_size=64)
-test_loader = DataLoader(test_dataset, batch_size=64)
-
-# Encoder
-class Encoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, n_layers):
-        super(Encoder, self).__init__()
-        self.n_layers = n_layers
-
-        mp_layers = [GINConv(
-            nn.Sequential(nn.Linear(input_dim, hidden_dim), nn.ReLU(), nn.BatchNorm1d(hidden_dim),
-                       nn.Linear(hidden_dim, hidden_dim), nn.ReLU()))]
-
-        for i in range(n_layers-1):
-            mp_layers.append(GINConv(
-                nn.Sequential(nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), nn.BatchNorm1d(hidden_dim),
-                           nn.Linear(hidden_dim, hidden_dim), nn.ReLU())))
-
-        self.mp = nn.ModuleList(mp_layers)
-
-    def forward(self, x_in, edge_index, batch):
-        x = self.mp[0](x_in, edge_index)
-        for i in range(1,self.n_layers):
-            x = self.mp[i](x, edge_index)
-
-        x_g = scatter_add(x, batch, dim=0)
-        return x_g, x
-
-# Decoder
-class Decoder(nn.Module):
-    def __init__(self, hidden_dim, n_layers, n_nodes):
-        super(Decoder, self).__init__()
-        self.n_layers = n_layers
-        self.n_nodes = n_nodes
-
-        mlp_layers = [nn.Linear(hidden_dim, hidden_dim) for i in range(n_layers-1)]
-        mlp_layers.append(nn.Linear(hidden_dim, 2*n_nodes*(n_nodes-1)//2))
-
-        self.mlp = nn.ModuleList(mlp_layers)
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        for i in range(self.n_layers-1):
-            x = self.relu(self.mlp[i](x))
-        
-        x = self.mlp[self.n_layers-1](x)
-        x = torch.reshape(x, (x.size(0), -1, 2))
-        x = F.gumbel_softmax(x, tau=1, hard=True)[:,:,0]
-
-        adj = torch.zeros(x.size(0), self.n_nodes, self.n_nodes, device=x.device)
-        idx = torch.triu_indices(self.n_nodes, self.n_nodes, 1)
-        adj[:,idx[0],idx[1]] = x
-        adj = adj + torch.transpose(adj, 1, 2)
-        return adj
-
-
-# Autoencoder
-class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, n_layers, n_nodes):
-        super(AutoEncoder, self).__init__()
-        self.input_dim = input_dim
-        self.n_nodes = n_nodes
-        self.encoder = Encoder(input_dim, hidden_dim, n_layers)
-        self.decoder = Decoder(hidden_dim, n_layers, n_nodes)
-        self.fc = nn.Linear(hidden_dim, 1)
-        self.softsort = SoftSort()
-
-    def forward(self, data):
-        x_g, _ = self.encoder(data.x, data.edge_index, data.batch)
-        adj = self.decoder(x_g)
-        return adj
-
-    def encode(self, data):
-        x_g, _ = self.encoder(data.x, data.edge_index, data.batch)
-        return x_g
-
-    def decode(self, x_g):
-        adj = self.decoder(x_g)
-        return adj
-
-    def loss_function(self, data):
-        x_g, x  = self.encoder(data.x, data.edge_index, data.batch)
-        adj = self.decoder(x_g)
-
-        with torch.no_grad():
-            degs = torch.bmm(adj, torch.ones(adj.size(0), adj.size(1), 1, device=x.device)).squeeze()
-            #degs = torch.pow(degs, -0.5)
-            #N = torch.diag_embed(degs)
-            #L = torch.eye(adj.size(1), device=x.device) - torch.bmm(torch.bmm(N, adj), N)
-            L = torch.diag_embed(degs) - adj
-            _, EigVec = torch.linalg.eig(L)
-            EigVec = torch.real(EigVec)
-            h = EigVec[:,:,1:self.input_dim+1]
-            h = torch.reshape(h, (h.size(0)*h.size(1), h.size(2)))
-            
-            adj_reshaped = torch.reshape(adj, (adj.size(0)*adj.size(1), adj.size(2)))
-            idx = torch.nonzero(adj_reshaped)
-            batch = torch.reshape(torch.arange(adj.size(0), device=x.device).repeat(adj.size(1), 1).T, (-1, 1)).squeeze()
-            _, x_gen = self.encoder(h, idx.T, batch)
-        scores = self.fc(x_gen).squeeze()
-        scores = torch.reshape(scores, (adj.size(0), adj.size(1)))
-        P = self.softsort(scores)
-        
-        adj_perm = torch.einsum("abc,acd->abd", (P, adj))
-        adj_perm = torch.einsum("abc,adc->abd", (adj_perm, P))
-            
-        scores = self.fc(x).squeeze()
-        with torch.no_grad():
-            _, lens = torch.unique(data.batch, return_counts=True)
-            idx = torch.cat([torch.arange(lens[i], device=x.device)+(i*self.n_nodes) for i in range(lens.size(0))], dim=0)
-            scores = torch.zeros(self.n_nodes*x_g.size(0), device=x.device).scatter_(0, idx, scores)
-            scores = torch.reshape(scores, (x_g.size(0), -1))
-        P = self.softsort(scores)
-        adj_in = torch.reshape(data.adj, (-1, data.adj.size(1), data.adj.size(1)))
-        adj_perm_in = torch.einsum("abc,acd->abd", (P, adj_in))
-        adj_perm_in = torch.einsum("abc,adc->abd", (adj_perm_in, P))
-
-        loss = F.l1_loss(adj_perm, adj_perm_in)
-        return loss
-        
+# Argument parser
+parser = argparse.ArgumentParser(description='NeuralGraphGenerator')
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--dropout', type=float, default=0.0)
+parser.add_argument('--batch-size', type=int, default=32)
+parser.add_argument('--epochs-autoencoder', type=int, default=3000)
+parser.add_argument('--hidden-dim-autoencoder', type=int, default=64)
+parser.add_argument('--latent-dim', type=int, default=128)
+parser.add_argument('--n-max-nodes', type=int, default=50)
+parser.add_argument('--n-layers-autoencoder', type=int, default=2)
+parser.add_argument('--spectral-emb-dim', type=int, default=10)
+parser.add_argument('--variational-autoencoder', action='store_true', default=False)
+parser.add_argument('--epochs-denoise', type=int, default=2000)
+parser.add_argument('--timesteps', type=int, default=500)
+parser.add_argument('--hidden-dim-denoise', type=int, default=256)
+parser.add_argument('--n-layers_denoise', type=int, default=2)
+args = parser.parse_args()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-autoencoder = AutoEncoder(pos_enc_dim, hidden_dim, n_layers, n_nodes).to(device)
-optimizer = torch.optim.Adam(autoencoder.parameters(), lr=lr)
+
+# Construct dataset
+dataset = CustomDataset(k=args.spectral_emb_dim)
+
+# Slit into training, validation and test sets
+idx = np.random.permutation(len(dataset))
+train_dataset = [dataset[idx[i]] for i in range(int(0.8*idx.size))]
+val_dataset = [dataset[idx[i]] for i in range(int(0.8*idx.size), int(0.9*idx.size))]
+test_dataset = [dataset[idx[i]] for i in range(int(0.9*idx.size), idx.size)]
+
+# Dataloaders
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=False)
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, pin_memory=False)
+test_loader = DataLoader(test_dataset, batch_size=args.batch_size, pin_memory=False)
+
+if args.variational_autoencoder:
+    autoencoder = VariationalAutoEncoder(1, args.hidden_dim_autoencoder, args.latent_dim, args.n_layers_autoencoder, args.n_max_nodes).to(device)
+else:
+    autoencoder = AutoEncoder(1, args.hidden_dim_autoencoder, args.latent_dim, args.n_layers_autoencoder, args.n_max_nodes).to(device)
+
+optimizer = torch.optim.Adam(autoencoder.parameters(), lr=args.lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=500, gamma=0.1)
 
 # Train autoencoder
-for epoch in range(1, epochs+1):
-    loss_all = 0
-
-    for data in train_loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        loss = autoencoder.loss_function(data)
-        loss.backward()
-        loss_all += data.num_graphs * loss.item()
-        optimizer.step()
-    if epoch % 5 == 0:
-        print('Epoch: {:03d}, Train Loss: {:.7f}'.format(
-                epoch, loss_all))
-
-#################################################################################
-
-def extract(a, t, x_shape):
-    batch_size = t.shape[0]
-    out = a.gather(-1, t.cpu())
-    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
-
-
-# forward diffusion (using the nice property)
-def q_sample(x_start, t, noise=None):
-    if noise is None:
-        noise = torch.randn_like(x_start)
-
-    sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape)
-    sqrt_one_minus_alphas_cumprod_t = extract(
-        sqrt_one_minus_alphas_cumprod, t, x_start.shape
-    )
-
-    return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
-
-
-# Loss function for denoising
-def p_losses(denoise_model, x_start, t, noise=None, loss_type="l1"):
-    if noise is None:
-        noise = torch.randn_like(x_start)
-
-    x_noisy = q_sample(x_start=x_start, t=t, noise=noise)
-    predicted_noise = denoise_model(x_noisy, t)
+for epoch in range(1, args.epochs_autoencoder+1):
+    autoencoder.train()
+    train_loss_all = 0
+    train_count = 0
+    if args.variational_autoencoder:
+        train_loss_all_recon = 0
+        train_loss_all_kld = 0
     
-    if loss_type == 'l1':
-        loss = F.l1_loss(noise, predicted_noise)
-    elif loss_type == 'l2':
-        loss = F.mse_loss(noise, predicted_noise)
-    elif loss_type == "huber":
-        loss = F.smooth_l1_loss(noise, predicted_noise)
-    else:
-        raise NotImplementedError()
+    for data in train_loader:
+        data['adj'] = data['adj'].to(device)
+        data['mask'] = data['mask'].to(device)
+        #data['eigvec'] = data['eigvec'][:,:,:args.spectral_emb_dim].to(device)
+        degs = torch.bmm(data['adj'], torch.ones(data['adj'].size(0), data['adj'].size(1), 1, device=device))
+        optimizer.zero_grad()
+        if args.variational_autoencoder:
+            loss, recon, kld  = autoencoder.loss_function(data['adj'], degs, data['mask'])
+            train_loss_all_recon += data['adj'].size(0) * recon.item()
+            train_loss_all_kld += data['adj'].size(0) * kld.item()
+        else:
+            loss = autoencoder.loss_function(data['adj'], degs, data['mask'])
+        loss.backward()
+        train_loss_all += data['adj'].size(0) * loss.item()
+        train_count += data['adj'].size(0)
+        optimizer.step()
 
-    return loss
+    autoencoder.eval()
+    val_loss_all = 0
+    val_count = 0
+    if args.variational_autoencoder:
+        val_loss_all_recon = 0
+        val_loss_all_kld = 0
 
+    for data in val_loader:
+        data['adj'] = data['adj'].to(device)
+        data['mask'] = data['mask'].to(device)
+        #data['eigvec'] = data['eigvec'][:,:,:args.spectral_emb_dim].to(device)
+        degs = torch.bmm(data['adj'], torch.ones(data['adj'].size(0), data['adj'].size(1), 1, device=device))
+        if args.variational_autoencoder:
+            loss, recon, kld  = autoencoder.loss_function(data['adj'], degs, data['mask'])
+            val_loss_all_recon += data['adj'].size(0) * recon.item()
+            val_loss_all_kld += data['adj'].size(0) * kld.item()
+        else:
+            loss = autoencoder.loss_function(data['adj'], degs, data['mask'])
+        val_loss_all += data['adj'].size(0) * loss.item()
+        val_count += data['adj'].size(0)
 
-# Position embeddings
-class SinusoidalPositionEmbeddings(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
+    if epoch % 5 == 0:
+        if args.variational_autoencoder:
+            print('Epoch: {:04d}, Train Loss: {:.5f}, Train Reconstruction Loss: {:.2f}, Train KLD Loss: {:.2f}, Val Loss: {:.5f}, Val Reconstruction Loss: {:.2f}, Val KLD Loss: {:.2f}'.format(epoch, train_loss_all/train_count, train_loss_all_recon/train_count, train_loss_all_kld/train_count, val_loss_all/val_count, val_loss_all_recon/val_count, val_loss_all_kld/val_count))
+        else:
+            print('Epoch: {:04d}, Train Loss: {:.5f}, Val Loss: {:.5f}'.format(epoch, train_loss_all/train_count, val_loss_all/val_count))
 
-    def forward(self, time):
-        device = time.device
-        half_dim = self.dim // 2
-        embeddings = math.log(10000) / (half_dim - 1)
-        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
-        embeddings = time[:, None] * embeddings[None, :]
-        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
-        return embeddings
+    scheduler.step()
 
-
-# Denoise model
-class DenoiseNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, n_layers):
-        super(DenoiseNN, self).__init__()
-        self.n_layers = n_layers
-
-        self.time_mlp = nn.Sequential(
-            SinusoidalPositionEmbeddings(hidden_dim),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.GELU(),
-            nn.Linear(hidden_dim, hidden_dim),
-        )
-
-        mlp_layers = [nn.Linear(input_dim, hidden_dim)] + [nn.Linear(hidden_dim, hidden_dim) for i in range(n_layers-2)]
-        mlp_layers.append(nn.Linear(hidden_dim, input_dim))
-        self.mlp = nn.ModuleList(mlp_layers)
-
-        bn_layers = [nn.BatchNorm1d(hidden_dim) for i in range(n_layers-1)]
-        self.bn = nn.ModuleList(bn_layers)
-
-        self.relu = nn.ReLU()
-
-    def forward(self, x, t):
-        t = self.time_mlp(t)
-        for i in range(self.n_layers-1):
-            x = self.relu(self.mlp[i](x))+t
-            x = self.bn[i](x)
-        
-        x = self.mlp[self.n_layers-1](x)
-        return x
-
-timesteps = 300
+torch.save({
+    'state_dict': autoencoder.state_dict(),
+    'optimizer' : optimizer.state_dict(),
+}, 'autoencoder.pth.tar')
 
 # define beta schedule
-betas = linear_beta_schedule(timesteps=timesteps)
+betas = linear_beta_schedule(timesteps=args.timesteps)
 
 # define alphas 
 alphas = 1. - betas
@@ -282,29 +131,57 @@ sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
 # calculations for posterior q(x_{t-1} | x_t, x_0)
 posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
 
-epochs = 5000
-
-denoise_model = DenoiseNN(input_dim=hidden_dim, hidden_dim=256, n_layers=3).to(device)
-optimizer = torch.optim.Adam(denoise_model.parameters(), lr=0.001)
+denoise_model = DenoiseNN(input_dim=args.latent_dim, hidden_dim=args.hidden_dim_denoise, n_layers=args.n_layers_denoise).to(device)
+optimizer = torch.optim.Adam(denoise_model.parameters(), lr=args.lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=500, gamma=0.1)
 
 # Train denoising model
-for epoch in range(epochs):
-    loss_all = 0
+for epoch in range(1, args.epochs_denoise+1):
+    denoise_model.train()
+    train_loss_all = 0
+    train_count = 0
     for data in train_loader:
-        data = data.to(device)
+        data['adj'] = data['adj'].to(device)
+        data['mask'] = data['mask'].to(device)
+        #data['eigvec'] = data['eigvec'][:,:,:args.spectral_emb_dim].to(device)
+        degs = torch.bmm(data['adj'], torch.ones(data['adj'].size(0), data['adj'].size(1), 1, device=device))
         optimizer.zero_grad()
-
-        batch_size = data.num_graphs
-        data = data.to(device)
-
-        x_g = autoencoder.encode(data)
-
-        t = torch.randint(0, timesteps, (batch_size,), device=device).long()
-        loss = p_losses(denoise_model, x_g, t, loss_type="huber")
+        x_g = autoencoder.encode(data['adj'], degs, data['mask'])
+        t = torch.randint(0, args.timesteps, (data['adj'].size(0),), device=device).long()
+        loss = p_losses(denoise_model, x_g, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, loss_type="huber")
         loss.backward()
-        loss_all += data.num_graphs * loss.item()
+        train_loss_all += data['adj'].size(0) * loss.item()
+        train_count += data['adj'].size(0)
         optimizer.step()
 
-    if epoch % 100 == 0:
-        print('Epoch: {:03d}, Train Loss: {:.7f}'.format(
-                epoch, loss_all))
+    denoise_model.eval()
+    val_loss_all = 0
+    val_count = 0
+    for data in val_loader:
+        data['adj'] = data['adj'].to(device)
+        data['mask'] = data['mask'].to(device)
+        #data['eigvec'] = data['eigvec'][:,:,:args.spectral_emb_dim].to(device)
+        degs = torch.bmm(data['adj'], torch.ones(data['adj'].size(0), data['adj'].size(1), 1, device=device))
+        x_g = autoencoder.encode(data['adj'], degs, data['mask'])
+        t = torch.randint(0, args.timesteps, (data['adj'].size(0),), device=device).long()
+        loss = p_losses(denoise_model, x_g, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, loss_type="huber")
+        val_loss_all += data['adj'].size(0) * loss.item()
+        val_count += data['adj'].size(0)
+
+    if epoch % 5 == 0:
+        print('Epoch: {:04d}, Train Loss: {:.5f}, Val Loss: {:.5f}'.format(epoch, train_loss_all/train_count, val_loss_all/val_count))
+
+    scheduler.step()
+
+torch.save({
+    'state_dict': denoise_model.state_dict(),
+    'optimizer' : optimizer.state_dict(),
+}, 'denoise_model.pth.tar')
+
+# sample 64 graphs
+samples = sample(denoise_model, latent_dim=args.latent_dim, timesteps=args.timesteps, betas=betas, batch_size=args.batch_size)
+
+# show a random one
+random_index = 5
+x_g_denoise = autoencoder.decode(samples[-1])
+print(torch.sum(x_g_denoise[random_index,:,:], dim=1))
